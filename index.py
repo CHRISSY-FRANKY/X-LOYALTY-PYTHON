2589759e3b82c62528dc62dc372d045273674a7f
Christopher Francisco <cjfg00gl3@gmail.com>
2025-03-29 15:20:34 -0400

    UPDATE index.py to render index.html instead of welcome message

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 from routes import main_routes # import the route manager
+from flask import render_template # render your html files
 
 @main_routes.route('/') # create the index route
 def index(): 
-    return "Welcome to XLoyalty!" # render the intro page
+    return render_template("index.html") # render the intro page
