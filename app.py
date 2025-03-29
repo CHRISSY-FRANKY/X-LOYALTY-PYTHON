79fbcdb0ae3a793fe47d9259f93c9de8af715986
Christopher Francisco <cjfg00gl3@gmail.com>
2025-03-29 14:47:03 -0400

    UPDATE app.py to register the main routes blueprint to the app

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask # import libraries
 import routes
+import index
 
 app = Flask(__name__) # create flask application instance
 
 if "__main__" == __name__:
+    app.register_blueprint(routes.main_routes) # add the routes
     app.run() # run flask instance
