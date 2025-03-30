5dfb0899c43c2d357f8852a1c1ef32d1e93967ca
Christopher Francisco <cjfg00gl3@gmail.com>
2025-03-30 14:15:35 -0400

    UPDATE script.js to add/remove viewport-less-than-screen to the form button/input selectors based on the size of the window.innerWidth

--- static/script.js
+++ static/script.js
@@ -2,11 +2,15 @@ function reviewViewportSize() { // Function to review viewport width is less tha
     if (window.innerWidth < (0.5 * window.screen.width)) {
         document.querySelector('header').classList.add('viewport-less-than-screen');
         document.querySelector('form').classList.add('viewport-less-than-screen');
+        document.querySelector('form button').classList.add('viewport-less-than-screen');
+        document.querySelector('form input').classList.add('viewport-less-than-screen');
         document.body.style.display = 'block'; 
         document.body.style.flexDirection = 'column';
     } else {
         document.querySelector('header').classList.remove('viewport-less-than-screen');
         document.querySelector('form').classList.remove('viewport-less-than-screen');
+        document.querySelector('form button').classList.remove('viewport-less-than-screen');
+        document.querySelector('form input').classList.remove('viewport-less-than-screen');
         document.body.style.display = 'flex';
         document.body.style.flexDirection = 'row';
     }
