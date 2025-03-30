b80b16e947a3c6d70b2cd966148f854352514425
Christopher Francisco <cjfg00gl3@gmail.com>
2025-03-30 13:42:59 -0400

    CREATE script.js from https://github.com/CHRISSY-FRANKY/X-LOYALTY-C-SHARP

---
+++ static/script.js
@@ -0,0 +1,17 @@
+function reviewViewportSize() { // Function to review viewport width is less than 0.66 screen width
+    if (window.innerWidth < (0.5 * window.screen.width)) {
+        document.querySelector('header').classList.add('viewport-less-than-screen');
+        document.querySelector('form').classList.add('viewport-less-than-screen');
+        document.body.style.display = 'block'; 
+        document.body.style.flexDirection = 'column';
+    } else {
+        document.querySelector('header').classList.remove('viewport-less-than-screen');
+        document.querySelector('form').classList.remove('viewport-less-than-screen');
+        document.body.style.display = 'flex';
+        document.body.style.flexDirection = 'row';
+    }
+}
+
+reviewViewportSize(); // Run on load
+
+window.addEventListener('resize', reviewViewportSize); // Run on resize
