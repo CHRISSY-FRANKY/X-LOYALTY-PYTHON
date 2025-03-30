7713a7012c173a5577223ec640360a99eba32718
Christopher Francisco <cjfg00gl3@gmail.com>
2025-03-30 16:12:34 -0400

    UPDATE index.py to /routes/index.py

--- routes/index.py
+++ routes/index.py
@@ -1,4 +1,4 @@
-from routes import main_routes # import the route manager
+from main_routes import main_routes # import the route manager
 from flask import render_template # render your html files
 
 @main_routes.route('/') # create the index route
