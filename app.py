a0c7c2f89c98c888fc7710f304662002b9c6420a
Christopher Francisco <cjfg00gl3@gmail.com>
2025-03-30 16:13:39 -0400

    UPDATE app.py to behave properly after changing routes.py to main_routes.py and moving index.py to /routes/index.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask # import libraries
-import routes
-import index
+import main_routes
+from routes.index import index as index
 
 app = Flask(__name__) # create flask application instance
 
 if "__main__" == __name__:
-    app.register_blueprint(routes.main_routes) # add the routes
+    app.register_blueprint(main_routes.main_routes) # add the routes
     app.run() # run flask instance
