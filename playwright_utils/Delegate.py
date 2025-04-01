9633d84b1038c36ec715c52314f9e316294b4c77
Christopher Francisco <cjfg00gl3@gmail.com>
2025-04-01 19:21:34 -0400

    UPDATE Delegate.py by implementing a constructor method to initialise the playwright variable

--- playwright_utils/Delegate.py
+++ playwright_utils/Delegate.py
@@ -1,9 +1,15 @@
+from playwright.sync_api import sync_playwright
+
 class Playwright_Delegate:
-    
+
     _instance = None # singleton
+    playwright = None
 
     @classmethod
     def get_instance(cls): # returns an instance of the caller that is saved in the caller
         if cls._instance is None:
             cls._instance = cls()
         return cls._instance
+    
+    def __init__(self):
+        self._playwright = sync_playwright().start()
