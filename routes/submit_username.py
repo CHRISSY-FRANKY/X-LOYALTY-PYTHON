cb7417f95529938efce18f1b784a0a98a79ddeeb
Christopher Francisco <cjfg00gl3@gmail.com>
2025-04-01 16:52:49 -0400

    UPDATE submit_username.py by creating verify_username as an empty function

--- routes/submit_username.py
+++ routes/submit_username.py
@@ -1,8 +1,13 @@
 from main_routes import main_routes # import the route manager 
 from flask import request # request fields
+from flask import render_template
+
+def verify_username(username) -> bool:
+   pass
 
 @main_routes.route("/submit_username", methods=["POST"]) # create the submit_username route 
 def submit_username():
     username = request.form["username"]
     print(username) # print the username
-    return "<div>^-^</div>"
+    print(verify_username(username))
+    return "^-^"
