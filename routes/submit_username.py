7d784eaa06012238cf4b89155fd8eb2b1ea49eb1
Christopher Francisco <cjfg00gl3@gmail.com>
2025-04-01 19:44:03 -0400

    UPDATE submit_username.py to load an x.com username profile

--- routes/submit_username.py
+++ routes/submit_username.py
@@ -1,6 +1,7 @@
 from main_routes import main_routes # import the route manager 
 from flask import request # request fields
 from flask import render_template
+from playwright_utils.Delegate import Playwright_Delegate
 
 def verify_username(username) -> bool:
    pass
@@ -10,4 +11,8 @@ def submit_username():
     username = request.form["username"]
     print(username) # print the username
     print(verify_username(username))
-    return "^-^"
+    page = Playwright_Delegate.get_instance().load_page(f"https://x.com/{username}")
+    if page is None:
+        return render_template("username_existant.html")
+    else:
+        return render_template("username_error.html")
