b97ebd9a3db37d6cc41d0d46e80087ab6823096b
Christopher Francisco <cjfg00gl3@gmail.com>
2025-04-10 14:20:52 -0400

    UPDATE submit_username.py by updating verify_username to return whether or not a username exists for the local computer

--- routes/submit_username.py
+++ routes/submit_username.py
@@ -4,20 +4,26 @@ from flask import render_template
 from playwright_utils.Delegate import Playwright_Delegate
 
 def verify_username(username) -> bool:
-   pass
+    page = Playwright_Delegate.get_instance().load_page(f"https://x.com/{username}") # load the username
+    try:
+        page.wait_for_selector('text="This account doesn’t exist"', timeout=10000) 
+        return False  # username doesn't exist
+    except Exception as e:
+        try:
+            page.wait_for_selector('text="Sign in to X"', timeout=10000) 
+            return True  # username exists
+        except Exception as e:
+            print(f"An error occured: {e}")
+            return False  # connection is slow or something else
 
 @main_routes.route("/submit_username", methods=["POST"]) # create the submit_username route 
 def submit_username():
-    username = request.form["username"]
+    username = request.form["username"] # get the username
     print(username) # print the username
-    print(verify_username(username))
-    page = Playwright_Delegate.get_instance().load_page(f"https://x.com/{username}")
-
-    #page.wait_for_selector('text="This account doesn’t exist"') # waits to see if the user doesn't exist
-    page.wait_for_selector('text="Sign in to X"') # waits to see if the user does exist
-
-    print("DONE")
-    if page is not None:
+    username_exists = verify_username(username) # verify username
+    if username_exists:
+        print("USERNAME EXISTS!")
         return render_template("username_existant.html")
     else:
+        print("USERNAME INEXISTANT!")
         return render_template("username_error.html")
