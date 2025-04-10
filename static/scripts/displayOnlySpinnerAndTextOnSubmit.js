552de361a5ddab91c58dba6b053f29b0a12a164f
Christopher Francisco <cjfg00gl3@gmail.com>
2025-04-10 15:46:02 -0400

    UPDATE index.html by CREATE displayOnlySpinnerAndTextOnSubmit.js by extracting the script into a seperate file

---
+++ static/scripts/displayOnlySpinnerAndTextOnSubmit.js
@@ -0,0 +1,22 @@
+document
+    .getElementById("submit_button")
+    .addEventListener("click", function (event) {
+        // Show only text and the spinner immediately when the button is clicked
+        const form = document.getElementById("username-form");
+        const formChildren = form.children;
+        for (let child of formChildren) {
+            if (
+                child.classList.contains("spinner") ||
+                child.classList.contains("stay")
+            ) {
+                child.style.display = "block"; // Explicitly show spinner and stay elements
+                if (child.id == "message") {
+                    const usernameInput = form.querySelector("input[name='username']");
+                    usernameText = usernameInput.value;
+                    child.innerHTML = "Searching for " + usernameText;
+                }
+            } else {
+                child.style.display = "none"; // Hide everything else
+            }
+        }
+    });
