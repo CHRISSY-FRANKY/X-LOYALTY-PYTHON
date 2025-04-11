f5895bf145b1f8c8cf2bd7b04b68ffb7f40302b8
Christopher Francisco <cjfg00gl3@gmail.com>
2025-04-10 21:17:17 -0400

    REMOVE followers_following.py file as it is no longer needed for managing follower and following routes, streamlining the codebase.

--- routes/followers_following.py
+++
@@ -1,5 +0,0 @@
-from main_routes import main_routes # import the route manager 
-from flask import request # request fields
-from flask import render_template
-from playwright_utils.Delegate import Playwright_Delegate
-import logging
