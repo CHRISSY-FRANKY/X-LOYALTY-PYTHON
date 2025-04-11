8ca1d2931132c7a9065ba3006420dc917a249965
Christopher Francisco <cjfg00gl3@gmail.com>
2025-04-10 22:08:27 -0400

    ADD signin_username route to app.py for integrating username sign-in functionality, enhancing route management in the application.

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask # import libraries
 import main_routes
 from routes.index import index as index
 from routes.submit_username import submit_username as submit_username
+from routes.signin_username import signin_username as signin_username
 
 app = Flask(__name__) # create flask application instance
 
